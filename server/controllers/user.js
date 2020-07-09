61280d66c8f9268012d650cba6b39bd658766a23
ChristianPardede <christian.kodz@gmail.com>
2020-07-09 20:47:40 +0700

    User complete

---
+++ server/controllers/user.js
@@ -0,0 +1,68 @@
+const { User } = require('../models')
+const { generateToken } = require('../helpers/jwt')
+const { decryptPassword } = require('../helpers/bcrypt')
+class UserController {
+    static register(req, res, next) {
+        console.log(req.body);
+        const payload = {
+            email: req.body.email,
+            password: req.body.password
+        }
+        
+        User.create(payload)
+        .then(data => {
+            const access_token = generateToken(payload)
+            return res.status(201).json({
+                id: data.id,
+                email: data.email,
+                access_token
+            })
+        })
+        .catch(err=> {
+            console.log((err));
+            next (err)
+        })
+    }
+    
+    static login(req, res, next) {
+        const { email, password } = req.body
+        User.findOne({
+            where: {
+                email
+            }
+        })
+        .then(data=> {
+            if(data) {
+                if(decryptPassword(password, data.password)){
+                    const access_token = generateToken({
+                        id: data.id,
+                        email: data.email
+                    })
+                    return res.status(200).json({
+                        id: data.id,
+                        email: data.email,
+                        access_token
+                    })
+                } else {
+                    return next({
+                        name: 'Bad Request',
+                        errors: [{
+                            message: 'Invalid email/password'
+                        }]
+                    })
+                }
+            } else {
+                return next({
+                    name: 'Bad Request',
+                    errors: [{
+                        message: 'Invalid email/password'
+                    }]
+                })
+            }
+        })
+        .catch(err => {
+            next (err)
+        })
+    }
+}
+module.exports = UserController
